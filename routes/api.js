dfc7a5ed545a23076357d19be619d48af68dbe25
taykangsheng <kangsheng@gmail.com>
2015-11-29 15:24:58 +0800

    /feedback /feedback/rating

--- routes/api.js
+++ routes/api.js
@@ -6,105 +6,89 @@ var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 var connection = require('../database/database.js');
 
-router.get('/recommendations', function(req,res){
-	console.log("RESTFUL API: \t recommendations");
-	authToken = req.headers.authToken;
-	if (authenticateToken(authToken)){
-		// authenticate success
-
-	} else{
-		// return error
-	}
+/*
+connection.query('INSERT into feedback (score, comment, userID, book) values (?,?,?,?);', [10, "nil", 4, '9780007179732'], function(err,results) {
+	if (err) throw err;
+	console.log(results);
 });
-
-router.get('/books', function(req,res){
-	console.log("RESTFUL API: \t books");
-	authToken = req.headers.authToken;
-	search = req.params.search;
-	if (authenticateToken(authToken)){
-		// authenticate success
-	} else{
-		// return error
-	}
-});
+*/
 
 router.get('/book', auth, function(req,res){
 	console.log("RESTFUL API: \t book");
-	ISBN = req.query.ISBN;
+	isbn13 = req.query.isbn13;
 
 	responseMessage = {};
-	connection.query('select * from book where ISBN13 LIKE ?;', [ISBN] , function(err, rows, fields) {	
-		if (err) throw err;
-		
-		if (rows.length == 0){
-			responseMessage.status = 0;
-			res.send(responseMessage);
-		} else{
-			responseMessage = rows[0];
-			responseMessage.status = 1;
-			connection.query('select fbID,date,score,comment,userID from feedback where book like ?;', [ISBN] , function(err, rows, fields) {
-				if (err) throw err;
-				if (rows.length == 0){
-					responseMessage.feedback = [];
-					res.send(responseMessage);
-				} else{
-					responseMessage.feedback = rows
-					res.send(responseMessage);
-				}
-			};
-		}
-	});
+	try{
+		connection.query('select * from book where ISBN13 LIKE ?;', [isbn13] , function(err, rows, fields) {	
+			if (err) throw err;
+			
+			if (rows.length == 0){
+				responseMessage.status = 0;
+				res.send(responseMessage);
+			} else{
+				responseMessage = rows[0];
+				responseMessage.status = 1;
+				connection.query('select fbID,date,score,comment,userID from feedback where book like ?;', [isbn13] , function(err, rows, fields) {
+					if (err) throw err;
+					if (rows.length == 0){
+						responseMessage.feedback = [];
+						res.send(responseMessage);
+					} else{
+						responseMessage.feedback = rows;
+						res.send(responseMessage);
+					}
+				});
+			}
+		});
+	} catch (err){
+		console.log(err);
+		responseMessage.status = 0;
+		res.send(responseMessage);
+	}
 });
 
-router.post('/feedback/:feedback_id', function (req, res) {
+
+router.post('/feedback', auth,function (req, res) {
 	console.log("RESTFUL API: \t feedback");
+	isbn13 = req.query.isbn13;
+	user = req.query.user;
+	score = req.query.score;
+	comment = req.query.comment;
 
-	authToken = req.headers.authToken;
-	
-	if (authenticateToken(authToken)){
-		// authenticate success
-	} else{
-		// return error
+	responseMessage = {};
+	try{
+		connection.query('INSERT into feedback (score, comment, userID, book) values (?,?,?,?);', [score, comment, user, isbn13], function(err,rows, fields) {
+			if (err) throw err;
+			responseMessage.status = 1;
+			res.send(responseMessage);
+		});
+	} catch (err){
+		console.log(err);
+		responseMessage.status = 0;
+		res.send(responseMessage);
 	}
-
 });
 
-router.post('/cart', function (req, res) {
-	console.log("RESTFUL API: \t cart");
+router.post('/feedback/rating', auth,function (req, res) {
+	console.log("RESTFUL API: \t feedback/rating");
+	user = req.query.user;
+	feedback = req.query.feedback;
+	rating = req.query.rating;
 
-	authToken = req.headers.authToken;
-	
-	if (authenticateToken(authToken)){
-		// authenticate success
-	} else{
-		// return error
+	responseMessage = {};
+	try{
+		connection.query('INSERT into rating (usefulness, fbID, userID) values (?,?,?);', [rating, feedback, user], function(err, rows, fields) {
+			if (err) throw err;
+			responseMessage.status = 1;
+			res.send(responseMessage);
+		});
+	} catch (err){
+		console.log(err);
+		responseMessage.status = 0;
+		res.send(responseMessage);
 	}
-
 });
 
 
-router.post('/order', function (req, res) {
-	console.log("RESTFUL API: \t order");
-	authToken = req.headers.authToken;
-
-	if (authenticateToken(authToken)){
-		// authenticate success
-	} else{
-		// return error
-	}
-
-});
-
-router.get('/user', function (req, res) {
-	console.log("RESTFUL API: \t user");
-	authToken = req.headers.authToken;
-	
-	if (authenticateToken(authToken)){
-		// authenticate success
-	} else{
-		// return error
-	}
-
-});
 
 module.exports = router;

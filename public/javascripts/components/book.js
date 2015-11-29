717b781a8fd36805f32bec01361c873cd5261867
tanhaoqin <tanhq646@gmail.com>
2015-11-30 00:21:52 +0800

    Added default selector for select in book page

--- public/javascripts/components/book.js
+++ public/javascripts/components/book.js
@@ -9,24 +9,35 @@
 
 		$scope.init = function(){
 			$scope.bookId = $stateParams.bookId
+			$scope.qtySelected = 1;
 			dataservice.getBook($scope.bookId, function(res){
 				$scope.book = res;
-				$scope.qty = 1;
-				$scope.displayedPrice = parseFloat($scope.book.price) * $scope.qty;
-				console.log(res);
+				$scope.bookCopies = []
+				for (var i = 1; i < $scope.book.copies + 1; i++){
+					$scope.bookCopies.push(i);
+				}
+				console.log($scope.bookCopies);
+				$scope.displayedPrice = parseFloat($scope.book.price) * $scope.qtySelected;
 			});
-		}
+			$scope.feedbackCount = 5;
+		};
 
-		$scope.updateQuantity = function(qty){
-			$scope.qty = qty;
-			$scope.displayedPrice = (parseFloat($scope.book.price) * $scope.qty).toFixed(2);
-		}
+		$scope.getFeedback = function(){
+			dataservice.getFeedback($scope.feedbackCount, function(res){
+				$scope.feedbacks = res;
+			});
+		};
+
+		$scope.updateQuantity = function(){
+			console.log($scope.qty);
+			$scope.displayedPrice = (parseFloat($scope.book.price) * $scope.qtySelected).toFixed(2);
+		};
+
+		$scope.init();
 
 		$scope.range = function(num){
 			return new Array(num);
-		}
-
-		$scope.init();
+		};
 
 	}]);
 

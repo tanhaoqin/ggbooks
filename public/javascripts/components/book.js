6cbaddbfa4064fecf7414277b54a9df4a699878e
tanhaoqin <tanhq646@gmail.com>
2015-12-01 02:21:52 +0800

    Added feedback section on book page

--- public/javascripts/components/book.js
+++ public/javascripts/components/book.js
@@ -16,7 +16,7 @@
 				for (var i = 1; i < $scope.book.copies + 1; i++){
 					$scope.bookCopies.push(i);
 				}
-				console.log($scope.bookCopies);
+				console.log($scope.book);
 				$scope.displayedPrice = parseFloat($scope.book.price) * $scope.qtySelected;
 			});
 			$scope.feedbackCount = 5;
@@ -36,8 +36,21 @@
 		$scope.init();
 
 		$scope.range = function(num){
+			console.log(num);
 			return new Array(num);
 		};
+
+		$scope.countFullStars = function(num){
+			return new Array(parseInt(num/2));
+		}
+
+		$scope.countHalfStars = function(num){
+			return new Array(parseInt(num%2));
+		}
+
+		$scope.countEmptyStars = function(num){
+			return new Array(5 - parseInt(parseInt(num)/2) - parseInt(num%2));
+		}
 
 	}]);
 

87c49e122228c87c8031f43a6aaebff2242819f8
Nikhil Sharma <me@nikhil.sh>
2015-12-04 19:22:59 +0800

    added GET book to search controller

--- public/javascripts/components/search.js
+++ public/javascripts/components/search.js
@@ -3,17 +3,26 @@
 
 	search.controller('SearchCtrl' , [
 	'$scope',
+	'$state',
+	'$stateParams',
 	'auth',
 	'dataservice',
-	function($scope, auth, dataservice) {
+	function($scope, $state, $stateParams, auth, dataservice) {
 		$scope.update = function(){
 			$scope.currentUser = auth.currentUser();
 			$scope.featured = dataservice.getSearchResults();
 		}
 		$scope.update();
+	  $scope.featuredBook = dataservice.featuredBook;
 
 		$scope.range = function(num){
 	  	return new Array(num);
 	  }
+
+	  $scope.goToBook = function(isbn13){
+	  	$stateParams.bookId = isbn13;
+	  	$state.go('book');
+	  }
+
 	}]);
 })();
--- public/javascripts/core/dataservice.js
+++ public/javascripts/core/dataservice.js
@@ -74,26 +74,28 @@
 
 		o.featuredBook = [];
 
-		
-
 		o.search = [{
 			author: "Neil Gaiman",
 			title: "American Gods",
 			rating: 4,
 			price: 10,
-			imageUrl: "http://spinoff.comicbookresources.com/wp-content/uploads/2011/03/american-gods.jpg" 
+			imageUrl: "http://spinoff.comicbookresources.com/wp-content/uploads/2011/03/american-gods.jpg",
+			isbn13: 9781405925136
+
 		},{
 			author: "Tan Hao Qin",
 			title: "How to get full marks in ML",
 			rating: 5,
 			price: 1000,
-			imageUrl: "http://img6a.flixcart.com/image/book/9/5/2/machine-learning-400x400-imadhxzhxbzjffa4.jpeg" 
+			imageUrl: "http://img6a.flixcart.com/image/book/9/5/2/machine-learning-400x400-imadhxzhxbzjffa4.jpeg",
+			isbn13: 9781405925136
 		},{
 			author: "J.K. Rowling",
 			title: "Hairy Pot",
 			rating: 3,
 			price: 20,
-			imageUrl: "http://www.jkrowling.com/uploads/images/large/en_US-timeline-image-harry-potter-and-the-deathly-hallows-1333632499.jpg" 
+			imageUrl: "http://www.jkrowling.com/uploads/images/large/en_US-timeline-image-harry-potter-and-the-deathly-hallows-1333632499.jpg",
+			isbn13: 9781405925136
 		}];
 
 		o.getSearchResults =  function() {

df7ed0d136a895a779c9dd5dcce5aa957bd1142e
taykangsheng <kangsheng@gmail.com>
2015-12-07 01:43:28 +0800

    registration done!

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,43 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/User');
+var connection = require('../database/database.js');
+
 // var jwt = require('express-jwt');
 
 // var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 router.post('/register', function(req, res, next){
-	if(!req.body.username || !req.body.password){
+	if(!req.body.password || !req.body.email || !req.body['credit_card'] ){
 		return res.status(400).json({message: "Please fill out all the fields"});
 	}
-	var user = new User();
-	user.username = req.body.username;
-	user.setPassword = req.body.password;
-	user.save(function (err){
-		if (err){
-			return next(err);
-		}
-		return res.json({token: user.generateJWT()});
-	});
+	
+	name = req.body.name;
+	password = req.body.password;
+	email = req.body.email;
+	credit_card = req.body['credit_card'];
+	phone = req.body.phone;
+	address = req.body.address;
+
+	responseMessage = {}
+	try{
+		connection.query('insert into user values (DEFAULT, ?, ?, ?);', [email, password, "u"], function(err,rows, fields) {
+			if (err) throw err;
+			
+			console.log("user inserted")
+		});
+		connection.query('insert into customer(userID,fullname,creditcard,phone,addressid) values((SELECT MAX(id) from user),?,?,?,?);', [name, credit_card, phone, address], function(err,rows, fields) {
+			if (err) throw err;
+			
+			console.log("customer inserted")
+		});
+		responseMessage.status = 1;
+		
+	} catch (err) {
+		responseMessage.status = 0;
+	}
+	responseMessage.userType = 'u';
+	res.send(responseMessage);
 });
 
 router.post('/login', function(req, res, next){

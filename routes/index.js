9182640c696f366650d7ef5333667166b8bb0c9e
taykangsheng <kangsheng@gmail.com>
2015-12-07 01:44:39 +0800

    register done

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.post('/register', function(req, res, next){
 	responseMessage.userType = 'u';
 	res.send(responseMessage);
 });
-
+ 
 router.post('/login', function(req, res, next){
 	if(!req.body.username || !req.body.password){
 		return res.status(400).json({message: "Please fill out all the fields"});

5cc6359675d74f31b79c163611ba565bb62b6c91
tanhaoqin <tanhq646@gmail.com>
2015-12-12 23:42:31 +0800

    Got homepage linked with backend

--- public/javascripts/components/home.js
+++ public/javascripts/components/home.js
@@ -14,15 +14,15 @@
   	  	  console.log(auth.isLoggedIn());
 		  if ($scope.isLoggedIn){
 		  	$scope.currentUser = auth.currentUser();
-		  	$scope.featured = dataservice.topSellers;
-		  	// dataservice.getRecommendations(function(res){
-		  	// 	if (res.user.length < 6){
-		  	// 		var slack = 6 - res.user.length;
-		  	// 		dataservice.getPopularBooks(slack, function(res){
-		  	// 			console.log(res);
-		  	// 		});
-		  	// 	} 
-		  	// });
+		  	// $scope.featured = dataservice.topSellers;
+		  	dataservice.getRecommendations(function(res){
+		  		if (res.user.length < 6){
+		  			var slack = 6 - res.user.length;
+		  			dataservice.getPopularBooks(slack, function(res){
+		  				$scope.featured = res.books;
+		  			});
+		  		} 
+		  	});
 		  } else {
 		  	$scope.featured = dataservice.topSellers;
 		  }	  	
